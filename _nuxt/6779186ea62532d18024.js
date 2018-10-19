b4ff78fc3a1be64f2f2b6c53a4ab430dfa36d19f
Justineo <justice360@gmail.com>
2018-10-19 16:23:30 +0800

    update

---
+++ _nuxt/6779186ea62532d18024.js
@@ -0,0 +1,87 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[11],{
+
+/***/ 1492:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./node_modules/veui-loader/lib??ref--11!./pages/development/veui.vue?vue&type=template&id=ebb8754c&scoped=true&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _vm._m(0)}
+var staticRenderFns = [function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('article',{staticClass:"content post"},[_c('h1',{attrs:{"id":"veui"}},[_vm._v("VEUI")]),_vm._v(" "),_c('div',{staticClass:"oss-badges custom-block"},[_c('p',[_c('a',{attrs:{"href":"https://github.com/ecomfe/veui"}},[_c('img',{attrs:{"alt":"VEUI on GitHub","src":"https://badgen.net/badge//GitHub?icon=github","width":"69","height":"20"}})]),_vm._v(" "),_c('a',{attrs:{"href":"https://travis-ci.org/ecomfe/veui"}},[_c('img',{attrs:{"alt":"VEUI on Travis CI","src":"https://travis-ci.org/ecomfe/veui.svg?branch=dev","width":"90","height":"20"}})]),_vm._v(" "),_c('a',{attrs:{"href":"https://www.npmjs.com/package/veui"}},[_c('img',{attrs:{"alt":"VEUI on npm","src":"https://badgen.net/npm/v/veui","height":"20"}})])])]),_vm._v(" "),_c('blockquote',[_c('p',[_vm._v("VEUI 是一套基于 "),_c('a',{attrs:{"href":"https://vuejs.org"}},[_vm._v("Vue.js")]),_vm._v(" 的、百度 ONE DESIGN 设计规范的组件化实现。")])]),_vm._v(" "),_c('p',[_vm._v("VEUI 采用组件库样式与逻辑分离的方式开发，用户可以灵活地指定甚至自行开发 VEUI 的主题包。VEUI 官方提供的默认主题包为 veui-theme-one。")]),_vm._v(" "),_c('p',[_vm._v("VEUI 是以 ES module 格式进行发布的，建议使用 Vue 官方项目模板初始化后配置 Babel 来进行转译。VEUI 除了提供丰富的组件，还提供多个功能强大的指令及插件，帮助开发者迅速搭建基于 Vue 的应用。")]),_vm._v(" "),_c('h4',{attrs:{"id":"浏览器兼容性"}},[_vm._v("浏览器兼容性")]),_vm._v(" "),_c('p',[_vm._v("VEUI 的功能和样式兼容 IE9 以上的现代浏览器。")])])}]
+
+
+// CONCATENATED MODULE: ./pages/development/veui.vue?vue&type=template&id=ebb8754c&scoped=true&
+
+// EXTERNAL MODULE: ./assets/styles/post.styl?vue&type=style&index=0&id=ebb8754c&lang=stylus&scoped=true&
+var postvue_type_style_index_0_id_ebb8754c_lang_stylus_scoped_true_ = __webpack_require__(986);
+
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(1);
+
+// CONCATENATED MODULE: ./pages/development/veui.vue
+
+var script = {}
+
+
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  script,
+  render,
+  staticRenderFns,
+  false,
+  null,
+  "ebb8754c",
+  null
+  
+)
+
+component.options.__file = "veui.vue"
+/* harmony default export */ var veui = __webpack_exports__["default"] = (component.exports);
+
+/***/ }),
+
+/***/ 589:
+/***/ (function(module, exports, __webpack_require__) {
+
+// style-loader: Adds some css to the DOM by adding a <style> tag
+
+// load the styles
+var content = __webpack_require__(987);
+if(typeof content === 'string') content = [[module.i, content, '']];
+if(content.locals) module.exports = content.locals;
+// add the styles to the DOM
+var add = __webpack_require__(6).default
+var update = add("f7e02626", content, true, {"sourceMap":false});
+
+/***/ }),
+
+/***/ 986:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var _node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_css_loader_index_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_7_oneOf_1_2_node_modules_stylus_loader_index_js_ref_7_oneOf_1_3_post_styl_vue_type_style_index_0_id_ebb8754c_lang_stylus_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(589);
+/* harmony import */ var _node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_css_loader_index_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_7_oneOf_1_2_node_modules_stylus_loader_index_js_ref_7_oneOf_1_3_post_styl_vue_type_style_index_0_id_ebb8754c_lang_stylus_scoped_true___WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_css_loader_index_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_7_oneOf_1_2_node_modules_stylus_loader_index_js_ref_7_oneOf_1_3_post_styl_vue_type_style_index_0_id_ebb8754c_lang_stylus_scoped_true___WEBPACK_IMPORTED_MODULE_0__);
+/* unused harmony reexport * */
+ /* unused harmony default export */ var _unused_webpack_default_export = (_node_modules_vue_style_loader_index_js_ref_7_oneOf_1_0_node_modules_css_loader_index_js_ref_7_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_7_oneOf_1_2_node_modules_stylus_loader_index_js_ref_7_oneOf_1_3_post_styl_vue_type_style_index_0_id_ebb8754c_lang_stylus_scoped_true___WEBPACK_IMPORTED_MODULE_0___default.a); 
+
+/***/ }),
+
+/***/ 987:
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(5)(false);
+// imports
+
+
+// module
+exports.push([module.i, "/*\n\nAtom One Dark by Daniel Gamage\nOriginal One Dark Syntax theme from https://github.com/atom/one-dark-syntax\n\nbase:    #282c34\nmono-1:  #abb2bf\nmono-2:  #818896\nmono-3:  #5c6370\nhue-1:   #56b6c2\nhue-2:   #61aeee\nhue-3:   #c678dd\nhue-4:   #98c379\nhue-5:   #e06c75\nhue-5-2: #be5046\nhue-6:   #d19a66\nhue-6-2: #e6c07b\n\n*/\n.hljs[data-v-ebb8754c] {\n  display: block;\n  overflow-x: auto;\n  padding: 0.5em;\n  color: #abb2bf;\n  background: #282c34;\n}\n.hljs-comment[data-v-ebb8754c],\n.hljs-quote[data-v-ebb8754c] {\n  color: #5c6370;\n  font-style: italic;\n}\n.hljs-doctag[data-v-ebb8754c],\n.hljs-keyword[data-v-ebb8754c],\n.hljs-formula[data-v-ebb8754c] {\n  color: #c678dd;\n}\n.hljs-section[data-v-ebb8754c],\n.hljs-name[data-v-ebb8754c],\n.hljs-selector-tag[data-v-ebb8754c],\n.hljs-deletion[data-v-ebb8754c],\n.hljs-subst[data-v-ebb8754c] {\n  color: #e06c75;\n}\n.hljs-literal[data-v-ebb8754c] {\n  color: #56b6c2;\n}\n.hljs-string[data-v-ebb8754c],\n.hljs-regexp[data-v-ebb8754c],\n.hljs-addition[data-v-ebb8754c],\n.hljs-attribute[data-v-ebb8754c],\n.hljs-meta-string[data-v-ebb8754c] {\n  color: #98c379;\n}\n.hljs-built_in[data-v-ebb8754c],\n.hljs-class .hljs-title[data-v-ebb8754c] {\n  color: #e6c07b;\n}\n.hljs-attr[data-v-ebb8754c],\n.hljs-variable[data-v-ebb8754c],\n.hljs-template-variable[data-v-ebb8754c],\n.hljs-type[data-v-ebb8754c],\n.hljs-selector-class[data-v-ebb8754c],\n.hljs-selector-attr[data-v-ebb8754c],\n.hljs-selector-pseudo[data-v-ebb8754c],\n.hljs-number[data-v-ebb8754c] {\n  color: #d19a66;\n}\n.hljs-symbol[data-v-ebb8754c],\n.hljs-bullet[data-v-ebb8754c],\n.hljs-link[data-v-ebb8754c],\n.hljs-meta[data-v-ebb8754c],\n.hljs-selector-id[data-v-ebb8754c],\n.hljs-title[data-v-ebb8754c] {\n  color: #61aeee;\n}\n.hljs-emphasis[data-v-ebb8754c] {\n  font-style: italic;\n}\n.hljs-strong[data-v-ebb8754c] {\n  font-weight: bold;\n}\n.hljs-link[data-v-ebb8754c] {\n  text-decoration: underline;\n}\n.post[data-v-ebb8754c] {\n  padding: 45px 60px;\n  font-size: 14px;\n  line-height: 1.8;\n  color: #666;\n  font-weight: 400;\n}\n.post-layout .post[data-v-ebb8754c] {\n  min-height: 100%;\n  margin-bottom: -192px;\n}\n.post-layout .post[data-v-ebb8754c]::after {\n  content: \"\";\n  display: block;\n  height: 192px;\n}\n.post-layout.no-links .post[data-v-ebb8754c] {\n  margin-bottom: -58px;\n}\n.no-links .post[data-v-ebb8754c]::after {\n  height: 58px;\n}\n.post h1[data-v-ebb8754c] {\n  margin-top: 0;\n  margin-bottom: 1.25em;\n  font-size: 36px;\n}\n.post h1 small[data-v-ebb8754c] {\n  vertical-align: 0.1em;\n  color: #999;\n}\n.post h2[data-v-ebb8754c] {\n  margin-top: 1.3em;\n  margin-bottom: 1.2em;\n  font-size: 30px;\n}\n.post h2[data-v-ebb8754c]::before {\n  content: \"#\";\n  margin-right: 5px;\n  color: #ccc;\n}\n.post h2 + h3[data-v-ebb8754c] {\n  margin-top: 2em;\n}\n.post h3[data-v-ebb8754c] {\n  margin-top: 1.25em;\n  margin-bottom: 1.15em;\n  font-size: 24px;\n}\n.post h4[data-v-ebb8754c] {\n  margin-top: 1.15em;\n  margin-bottom: 1.1em;\n  font-size: 18px;\n}\n.post h5[data-v-ebb8754c] {\n  margin-top: 1.05em;\n  margin-bottom: 1.05em;\n  font-size: 14px;\n}\n.post h6[data-v-ebb8754c] {\n  margin-top: 1em;\n  margin-bottom: 1em;\n  font-size: 12px;\n}\n.post h1[data-v-ebb8754c],\n.post h2[data-v-ebb8754c],\n.post h3[data-v-ebb8754c] {\n  font-weight: 500;\n  clear: both;\n}\n.post h1[data-v-ebb8754c],\n.post h2[data-v-ebb8754c],\n.post h3[data-v-ebb8754c],\n.post h4[data-v-ebb8754c],\n.post h5[data-v-ebb8754c],\n.post h6[data-v-ebb8754c] {\n  color: #333;\n  line-height: 1;\n}\n.post br[data-v-ebb8754c] {\n  clear: both;\n}\n.post p[data-v-ebb8754c] {\n  margin-top: 0.5em;\n  margin-bottom: 0.5em;\n}\n.post ul[data-v-ebb8754c],\n.post ol[data-v-ebb8754c] {\n  padding-left: 1.5em;\n}\n.post blockquote[data-v-ebb8754c] {\n  margin: 1em 0;\n  padding-left: 1em;\n  border-left: 5px solid #f1f1f1;\n  color: #999;\n}\n.post table[data-v-ebb8754c] {\n  width: 100%;\n  border-collapse: collapse;\n  border: 1px solid #f1f1f1;\n  border-style: none solid;\n  margin-top: 1.5em;\n  margin-bottom: 1.5em;\n}\n.post th[data-v-ebb8754c],\n.post td[data-v-ebb8754c] {\n  min-width: 90px;\n  padding: 7px 14px;\n  border: 1px solid #f1f1f1;\n  border-style: solid none;\n  text-align: left;\n}\n.post th[data-v-ebb8754c] > :first-child,\n.post td[data-v-ebb8754c] > :first-child {\n  margin-top: 5px;\n}\n.post th[data-v-ebb8754c] > :last-child,\n.post td[data-v-ebb8754c] > :last-child {\n  margin-bottom: 5px;\n}\n.post th pre[data-v-ebb8754c],\n.post td pre[data-v-ebb8754c] {\n  padding: 10px 15px;\n}\n.post th table[data-v-ebb8754c],\n.post td table[data-v-ebb8754c] {\n  margin-top: 0.2em;\n  margin-bottom: 0.5em;\n}\n.post td[data-v-ebb8754c]:first-child {\n  white-space: nowrap;\n}\n.post a[data-v-ebb8754c]:link:not([class^=\"veui-\"]),\n.post a[data-v-ebb8754c]:visited:not([class^=\"veui-\"]) {\n  text-decoration: none;\n  color: #3998fc;\n  transition: color 0.2s;\n}\n.post a[data-v-ebb8754c]:link:not([class^=\"veui-\"]):hover,\n.post a[data-v-ebb8754c]:visited:not([class^=\"veui-\"]):hover {\n  color: #3389e3;\n}\n.post a[data-v-ebb8754c]:link:not([class^=\"veui-\"]):active,\n.post a[data-v-ebb8754c]:visited:not([class^=\"veui-\"]):active {\n  color: #2e7aca;\n}\n.post code[data-v-ebb8754c] {\n  padding: 2px 4px;\n  background-color: rgba(0,0,0,0.024);\n  font-size: 90%;\n}\n.post hr[data-v-ebb8754c] {\n  height: 1px;\n  margin: 2em 0;\n  padding: 0;\n  background-color: #eff0f1;\n  border: 0;\n  clear: both;\n}\n.post figure[data-v-ebb8754c] {\n  float: right;\n  width: 60%;\n  margin: 0 0 30px 20px;\n}\n.post figure + h1[data-v-ebb8754c],\n.post figure + h2[data-v-ebb8754c],\n.post figure + h3[data-v-ebb8754c],\n.post figure + h4[data-v-ebb8754c],\n.post figure + h5[data-v-ebb8754c],\n.post figure + h6[data-v-ebb8754c] {\n  margin-top: 0;\n}\n.post figure img[data-v-ebb8754c] {\n  display: block;\n  margin: auto;\n}\n.post figure .preview[data-v-ebb8754c] {\n  padding: 20px;\n  overflow: hidden;\n  border: 1px solid #e5e5e5;\n  transition: box-shadow 0.2s;\n  cursor: pointer;\n  text-align: center;\n}\n.post figure .preview[data-v-ebb8754c]:hover {\n  box-shadow: 0 0 5px rgba(0,0,0,0.2);\n}\n.post figure.hero[data-v-ebb8754c] {\n  width: 100%;\n}\n.post figcaption[data-v-ebb8754c] {\n  margin-top: 10px;\n  font-size: 12px;\n}\n.post figcaption p[data-v-ebb8754c] {\n  margin: 0;\n  line-height: 20px;\n}\n.post figcaption .caption[data-v-ebb8754c] {\n  font-weight: 500;\n}\n.post figcaption .desc[data-v-ebb8754c] {\n  color: #999;\n}\n.post .comparison[data-v-ebb8754c] {\n  float: right;\n  width: 60%;\n  margin: 0 0 30px 20px;\n}\n.post .comparison figure[data-v-ebb8754c] {\n  width: calc(50% - 5px);\n  margin: 0;\n}\n.post .comparison .good[data-v-ebb8754c] {\n  float: left;\n}\n.post .comparison .bad[data-v-ebb8754c] {\n  float: right;\n}\n.post .comparison .bad .caption[data-v-ebb8754c] {\n  color: #ff5b5b;\n}\n.post img[data-v-ebb8754c] {\n  max-width: 100%;\n  max-height: 100%;\n}\n.post pre[data-v-ebb8754c] {\n  border: 1px solid #eee;\n  padding: 30px;\n  background-color: #f9f9f9;\n  white-space: pre;\n  overflow: auto;\n}\n.post pre code[data-v-ebb8754c] {\n  background-color: transparent;\n  padding: 0;\n}\n.post var[data-v-ebb8754c] {\n  font-family: \"PT Serif\", Georgia, serif;\n}\n.post kbd[data-v-ebb8754c] {\n  display: inline-block;\n  padding: 3px 5px 6px;\n  font-size: 90%;\n  line-height: 10px;\n  color: #444d56;\n  vertical-align: 1px;\n  background-color: #fafbfc;\n  border: solid 1px #c6cbd1;\n  border-bottom-color: #959da5;\n  border-radius: 3px;\n  box-shadow: inset 0 -1px 0 #959da5;\n}\n.post .custom-block[data-v-ebb8754c] {\n  border: 1px solid;\n  margin: 1em 0;\n  padding: 0.75em 1em;\n}\n.post .custom-block[data-v-ebb8754c] > :first-child {\n  margin-top: 0;\n}\n.post .custom-block[data-v-ebb8754c] > :last-child {\n  margin-bottom: 0;\n}\n.post .alert[data-v-ebb8754c],\n.post .warning[data-v-ebb8754c],\n.post .tip[data-v-ebb8754c] {\n  font-size: 13px;\n}\n.post .alert[data-v-ebb8754c] {\n  border-color: #fee;\n  background-color: #fff6f6;\n}\n.post .warning[data-v-ebb8754c] {\n  border-color: #fef4e6;\n  background-color: #fef9f2;\n}\n.post .tip[data-v-ebb8754c] {\n  border-color: #d8ebff;\n  background-color: #ebf5ff;\n}\n.post .badges[data-v-ebb8754c] {\n  border: none;\n  padding: 0;\n}\n.post .badges code[data-v-ebb8754c] {\n  padding: 2px 3px;\n  background: #333;\n  border-radius: 2px;\n  color: #fff;\n  font-size: 80%;\n}\n.post .oss-badges[data-v-ebb8754c] {\n  border: none;\n  padding: 0;\n}\n.post .oss-badges a[data-v-ebb8754c] {\n  margin-right: 10px;\n}\n.post .one-demo[data-v-ebb8754c] {\n  margin: 1em 0 1.25em;\n}\n", ""]);
+
+// exports
+
+
+/***/ })
+
+}]);
